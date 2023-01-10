af3cad86c1a6749907831fade6bf1b127359b5d0
Cyrus Ndirangu <cyrus_g@outlook.com>
2023-01-10 17:22:21 +0000

    Saw some errors on using React 17 methods in React 18, migrated app to React 18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
