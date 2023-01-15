3081d74f7f11f89383a078ddea4e9206bc30e674
NicholasWarui <nyamunick99@gmail.com>
2023-01-15 21:14:10 +0300

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,17 @@ import React from "react";
 import star from "../assets/images/Star 1.png";
 
 export default function Card(props) {
+  let badgeText
+  if(props.item.openSpots === 0){
+    badgeText = "SOLD OUT";
+  } 
+  else if(props.item.location === "Online"){
+    badgeText = "ONLINE"
+  }
+
   return (
     <div className="card">
-        {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+        {badgeText && <div className="card--badge">SOLD OUT</div>}
       <img src={`${props.img}`} alt="katie-img" className="card--image" />
       <div className="card--stats">
         <img src={star} alt="airbnb logo" className="card--star" />
